416b982e69d29a3434d46730674545e2e1d201cf
polo-sec <mattswann@protonmail.com>
2020-07-22 14:45:58 +0000

    Implemented trend pulling by Country and City

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
-import twima
+from twima import twima_ui
+from twima import site
 
-def query_details(query_type):
-    print("\n", "Select User Agent", "\n", "1. Firefox", "\n", "2. Chrome", "\n", "3. Safari", "\n")
-    user_input = input("> ")
-    if user_input == "1":
-        user_agent =
-    elif user_input == "2":
-        user_agent = 
-    elif user_input == "3":
-        user_agent == 
-    else: print("\n", "Please select a valid user agent")
-    print("\n")
-    print("Select User Agent")
-    print("\n")
+twima_instance = twima_ui()
 
-    #if query_type == "world":
-
-
-def main():
-    while True:
-        print("\n")
-        print(24*'-', " Welcome to Twima", 24*'-', "\n") 
-        print(12*' ',"The API-less Twitter manipulator by @Polo-Sec", "\n")
-        print("1 - View currently trending [worldwide]")
-        print("2 - View currently trending [by country]")
-        print("Q - Quit", "\n")
-        user_input = input("> ")
-        if user_input == "Q":
-            quit()
-            
-main()
-
+twima_instance.main_menu()
+menu_choice = twima_instance.get_input()
+if menu_choice == "Q":
+    exit()
+elif menu_choice == "1":
+    user_agent = twima_instance.set_user_agent()
+    source = twima_instance.set_source()
+    site_data = site(source,user_agent)
+    print("Trending Topics")
+    print(site_data.get_trending(),"\n")
+    print("Trending Hashtags")
+    print(site_data.get_hashtags(),"\n")
+elif menu_choice == "2":
+    user_agent = twima_instance.set_user_agent()
+    source = twima_instance.set_source()
+    twima_instance.set_location(user_agent,source)
--- twima.py
+++ twima.py
@@ -9,7 +9,6 @@ class site:
 
     def scraper(self):
         headers = {'User-Agent': self.user_agent}
-        print()
         response = requests.get(self.url,headers=headers)
         content = response.text
         return content
@@ -35,6 +34,68 @@ class site:
             clean = re.findall(clean_tags,item[1])
             taglist.append(clean[0][1])
         return taglist
+
+class twima_ui:
+
+    def __init__(self):
+        pass
+    
+    def main_menu(self):
+        print("\n")
+        print(24*'-', " Welcome to Twima", 24*'-', "\n") 
+        print(12*' ',"The API-less Twitter manipulator by @Polo-Sec", "\n")
+        print("1 - View currently trending [worldwide]")
+        print("2 - View currently trending [by country]")
+        print("Q - Quit")
+
+    def get_input(self):
+        print("\n")
+        user_input = input("> ")
+        print("\n")
+        return user_input
+
+    def set_user_agent(self):
+        available_user_agents = [
+                'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+                ]
+        print("Select User Agent")
+        print("\n", "1. Firefox", "\n", "2. Chrome", "\n", "3. Safari")
+        choice = int(self.get_input())
+        user_agent = available_user_agents[choice-1]
+        return user_agent
+    
+    def set_source(self):
+        sources = [
+                'https://getdaytrends.com'
+                ]
+        print("Select Data Source")
+        print("\n", "1. Get Day Trends [https://getdaytrends.com]")
+        choice = int(self.get_input())
+        source = sources[choice-1]
+        return source
+
+    def set_location(self,user_agent,url):
+        headers = {'User-Agent': user_agent}
+        print("Please enter the country")
+        country = self.get_input()
+        country = country.replace(' ','-').lower()
+        print("Would you like to specify a city? [Y/N]")
+        city_choice = self.get_input()
+        if city_choice == "Y":
+            print("Please enter the City")
+            city = self.get_input()
+            city = city.replace(' ','-').lower()
+            response = requests.get(url+'/'+country+'/'+city,headers=headers)
+            print(response)
+        else: 
+            response = requests.get(url+'/'+country,headers=headers)
+            print(response)
+
+            
+
+
 
 #daytrends = site('https://getdaytrends.com/','Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0')
 
